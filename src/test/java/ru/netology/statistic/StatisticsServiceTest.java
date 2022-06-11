92259d54f6f74ba6c74ea6daa1194c21b5284a98
Yana Bialkova <yana.bialkova@gmail.com>
2022-06-11 17:00:25 +0200

    Fhird commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class StatisticsServiceTest {
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatisticsServiceTest {
+
+    @Test
+    public void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected,actual);
+    }
 }
